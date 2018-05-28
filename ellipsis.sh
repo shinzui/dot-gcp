04f35758dec79638c5a21120f45f801be9097263
Nadeem Bitar <nadeem@gmail.com>
2018-05-27 22:35:51 -0700

    :package: Install sql proxy

--- ellipsis.sh
+++ ellipsis.sh
@@ -5,6 +5,15 @@
 pkg.install() {
   brew cask install google-cloud-sdk
   gcloud components install beta alpha gsutil pubsub-emulator
+
+  install_sql_proxy
+}
+
+install_sql_proxy() {
+  cd /tmp
+  curl -o cloud_sql_proxy https://dl.google.com/cloudsql/cloud_sql_proxy.darwin.amd64
+  chmod +x cloud_sql_proxy
+  mv cloud_sql_proxy /usr/local/bin/
 }
 
 # pkg.push() {
